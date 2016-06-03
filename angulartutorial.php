6ec9d642f07ba42fa16e076ad34244602a2535a5
Jesse Woo <woojesse@gmail.com>
2016-06-03 15:50:07 -0700

    Push Angular tutorial

--- angulartutorial.php
+++ angulartutorial.php
@@ -1,10 +1,21 @@
-<html>
+<!doctype html>
+<html ng-app>
  	<head>
+	    <title>Sample AngularJS Controller</title>
 	</head>
 	<body>
-		<div></div>
+	    <div ng-controller="TestCtrl">
+	        <h1>{{title}}</h1>
+	        <input type="text" ng-model="title">
+	    </div>
 
-		<script src="/3rd-party/jquery-1.12.4.min.js"></script>
-		<script src="/3rd-party/angular.min.js"></script>
-	</body>
+	    <script src="3rd-party/jquery-1.12.4.min.js"></script>
+	    <script src="3rd-party/angular.min.js"></script>
+
+	    <script>
+	      function TestCtrl($scope) {
+	        $scope.title = 'Write a title here...';
+	      };
+	    </script>
+	</body>	
 </html>
